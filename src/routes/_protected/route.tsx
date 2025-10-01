ea45d6187df89cdaa9cf07696511e40e21c9b34a
Kyler Riggs <100742516+ky-ler@users.noreply.github.com>
2025-10-01 01:50:29 -0400

    Remove extra items from layout

--- src/routes/_protected/route.tsx
+++ src/routes/_protected/route.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import {
   createFileRoute,
-  Link,
+  // Link,
   Outlet,
   redirect,
-  useRouter,
+  // useRouter,
 } from "@tanstack/react-router";
-import { useAuth } from "react-oidc-context";
+// import { useAuth } from "react-oidc-context";
 
 export const Route = createFileRoute("/_protected")({
   beforeLoad: ({ context, location }) => {
@@ -23,38 +23,5 @@ export const Route = createFileRoute("/_protected")({
 });
 
 function AuthLayout() {
-  const router = useRouter();
-  const navigate = Route.useNavigate();
-  const auth = useAuth();
-
-  const handleLogout = () => {
-    if (window.confirm("Are you sure you want to logout?")) {
-      auth.signoutSilent().then(() => {
-        router.invalidate().finally(() => {
-          navigate({ to: "/" });
-        });
-      });
-    }
-  };
-
-  return (
-    <div className="p-2 h-full">
-      <h1>Authenticated Route</h1>
-      <p>This route's content is only visible to authenticated users.</p>
-      <ul className="py-2 flex gap-2">
-        <li>
-          <Button variant={"link"} asChild>
-            <Link to="/">Dashboard</Link>
-          </Button>
-        </li>
-        <li>
-          <Button variant={"destructive"} onClick={handleLogout}>
-            Logout
-          </Button>
-        </li>
-      </ul>
-      <hr />
-      <Outlet />
-    </div>
-  );
+  return <Outlet />;
 }
